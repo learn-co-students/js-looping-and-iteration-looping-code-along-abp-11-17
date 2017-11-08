c509c8b23e4be9feb0bded483f5b11410e51a2c0
Quinn Dizon <qdizon@gmail.com>
2017-11-08 00:45:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const a = ['Ada', 'Brendan', 'Ali'];
+function printBadges(a){
+  for(let i = 0; i < a.length; i++){
+    console.log(`Welcome ${a[i]}! You are employee #${i+1}.`);
+  }
+  return a;
+}
+
+function tailsNeverFails(){
+  let streak = 0;
+  while (Math.random() >= 0.5){
+    streak++;
+  }
+  return `You got ${streak} tails in a row!`;
+}
