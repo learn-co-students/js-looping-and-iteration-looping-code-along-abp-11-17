d4e731cf2368bb63e9681f50b32264a9ad4a9941
Victoria Meng <nights1ststar@protonmail.com>
2017-11-08 03:26:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const names = ['Ada', 'Brendan', 'Ali']
+
+function printBadges(names) {
+  for (let counter = 0; counter < names.length; counter++) {
+    console.log(`Welcome ${names[counter]}! You are employee #${counter + 1}.`);
+  }
+  return names;
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails() {
+  let tails = 0;
+
+  while (maybeTrue()) {
+    tails++;
+  }
+
+  return `You got ${tails} tails in a row!`;
+}
