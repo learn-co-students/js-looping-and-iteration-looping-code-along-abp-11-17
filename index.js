3d01333e00e88ffbb5fb4956b7d83c1ba3852c5f
Helen Liutongco <bellpickle@gmail.com>
2017-11-08 02:27:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const gifts=["teddy bear","drone","doll"];
+
+function wrapGifts(gifts) {
+  for(let i=0;i<gifts.length;i++){
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+  return gifts;
+}
+
+function printBadges(employee) {
+  for(let i=0;i<employee.length;i++) {
+    console.log(`Welcome ${employee[i]}! You are employee #${i+1}.`);
+  }
+  return employee;
+}
+
+function tailsNeverFails() {
+  let counter=0;
+  while(Math.round(Math.random())){
+    counter++;
+  }
+  return `You got ${counter} tails in a row!`;
+}
