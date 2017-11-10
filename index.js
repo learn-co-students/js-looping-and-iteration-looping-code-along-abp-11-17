11d4f4554ae276d32b5b151c77c57107daf51b4b
Maverick Tesini <mavericktesini@gmail.com>
2017-11-10 00:29:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+function printBadges(string) {
+  const employees = ['Joe', 'Gabe', 'Lisa'];
+  var elength = employees.length;
+  for (i=0; i < elength; i++) {
+    console.log('Welcome ' + employees[i] + '!' + ' You are employee #' + (i+1) + '.');
+  }
+  return string;
+}
+
+function flip() {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails() {
+  let Heads = "heads";
+  let Tails = "tails";
+  var x = 0;
+  while (flip() ) {
+    ++x;
+  }
+  return 'You got ' + x + ' ' + Tails + ' in a row!';
+}
