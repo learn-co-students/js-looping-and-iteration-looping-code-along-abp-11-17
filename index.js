1155d7eb27a11bf98e6e3ec3569901c87dc048c3
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-11 22:04:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Code your solutions in this file
-let arr = ['bob', 'jill', 'rob'];
 function printBadges(arr) {
   for (let i = 0; i < arr.length; i++) {
-    console.log(`Welcome ${arr[i]}! You are employee #${arr}`);
+    console.log(`Welcome ${arr[i]}! You are employee #${i + 1}.`);
   }
+  return arr;
+}
+
+function tailsNeverFails() {
+  while (Math.random() >= 0.5) {}
 }
