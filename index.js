a6328efc0758f2e9e7ae26f22e06bff673f78638
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-11 19:25:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+let arr = ['bob', 'jill', 'rob'];
+function printBadges(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`Welcome ${arr[i]}! You are employee #${arr}`);
+  }
+}
