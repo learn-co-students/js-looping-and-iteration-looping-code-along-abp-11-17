3512a95732a64ceadde2178f864fad68bf337759
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-11 16:58:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+//employees = ["Ada", "Brendan", "Ali"]
+
+function printBadges(employees) {
+  for(let i = 0; i < employees.length; i++) {
+    console.log(`Welcome ${employees[i]}! You are employee #${i + 1}.`);
+  }
+  return employees;
+}
+
+//printBadges(employees);
+
+function maybeTrue () {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails() {
+  tails = 0;
+  while(maybeTrue()) {
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`
+}
