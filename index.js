ab8f69bdbdb8af319d087caab9e82cd5c01a4607
Beth Schofield <bethmschofield@gmail.com>
2017-11-13 18:58:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function printBadges(list) {
+  for (let i=0; i < list.length; i++) {
+    console.log(`Welcome ${list[i]}! You are employee #${i+1}.`)
+  }
+  return list
+}
+
+function coinToss() {
+  return Math.random() >= 0.5;
+}
+function tailsNeverFails() {
+  var tailCount = 0
+  while (coinToss()) {
+  tailCount++
+  }
+  return `You got ${tailCount} tails in a row!`
+}
