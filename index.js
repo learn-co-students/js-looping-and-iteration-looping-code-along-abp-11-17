dffe3fd90d9070b931f52b7d7e46f7aca3ca857e
matt hagner <mhagner77@gmail.com>
2017-11-13 00:26:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function printBadges(array){
+  let newArray = [...array];
+  for(let i = 0; i < array.length; i++){
+    console.log(`Welcome ${array[i]}! You are employee #${i+1}.`);
+  }
+  return array;
+}
+
+function tailsNeverFails(){
+  let number = Math.random();
+  let counter = 0;
+  while(number >= 0.5){
+    console.log('Tails')
+    number = Math.random();
+    counter++;
+  }
+  return `You got ${counter} tails in a row!`
+}
