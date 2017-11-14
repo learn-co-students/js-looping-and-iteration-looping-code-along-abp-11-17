b5624f45b9b164010ef4bf49b42d79e82a2c2a6c
David Kim <mail.ddk@gmail.com>
2017-11-14 06:04:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+function printBadges(arrayNames) {
+  // arrayNames = [`Lisa`, `Kaitlin`, `Jan`]
+  for (let i = 0; i < arrayNames.length; i++) {
+    // i
+    // arrayNames[i]
+    console.log(`Welcome ${arrayNames[i]}! You are employee #${i+1}.`)
+  }
+  return arrayNames
+}
+
+
+
+function coinToss() {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails(){
+  let counter = 0;
+  while(coinToss()) {
+    counter++
+  }
+  return `You got ${counter} tails in a row!`
+}
