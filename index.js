a82181e27e42202b2d3b1a66dd059ab67c7c213b
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-14 13:11:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const employee = ['Kaitln', 'Jan', 'Joe', 'Gabe', 'Lisa']
+
+
+function printBadges(employee) {
+  for (let i=0; i < employee.length; i++ ) {
+  console.log (`Welcome ${employee[i]}! You are employee #${i+1}.`)
+}
+return employee;
+}
+printBadges(employee);
+
+
+
+
+let tails= []
+
+function tailsNeverFails() {
+    return Math.random() >=0.5;
+     tails.push[tails]
+}
+
+//while (tailsNeverFails) {
+
+  //console.log (`You got ${i+1} tails in a row!`)
