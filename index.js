07568ed543cb870ed6848f9f17819540927b039f
Tim Shipman <bricklayertim@yahoo.com>
2017-11-14 17:33:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function printBadges(worker){
+  for (let i = 0; i < worker.length; i++) {
+    console.log(`Welcome ${worker[i]}! You are employee #${i+ 1}.`)
+  }
+  return worker;
+}
+
+function tailsNeverFails() {
+  let  tails = 0;
+  while (Math.random() >= 0.5){
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`;
+}
