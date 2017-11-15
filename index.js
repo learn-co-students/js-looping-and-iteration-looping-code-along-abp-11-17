18a4942349f68b86d6aa2618a8dce7d4764bf8f1
Patrick Bope <patbpm1@gmail.com>
2017-11-15 12:59:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+// Code your solutions in this fi
+function printBadges (name){
+  for (var i = 0; i < name.length; i++) {
+      console.log('Welcome ' + name[i]+ '! You are employee #' + (i+1) + '.');
+
+  }
+  return name;
+}
+printBadges(['Joe', 'Gabe']);
+
+function tailsNeverFails() {
+  return Math.random() <= 1;
+}
+
+while (tailsNeverFails()) {
+    console.log('You got 8 tails in a row!');
+}
