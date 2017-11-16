5961dd95371f2281c75077ff72faa4b355af3969
Anthony Catalfo <anthonycatalfo@gmail.com>
2017-11-16 05:01:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function printBadges(employeeNames) {
+  employeeNames.map((employeeName,index,employeeNames)=>{
+    console.log(`Welcome ${employeeNames[index]}! You are employee #${index+1}.`);
+
+  } )
+  return employeeNames;
+}
+function tailsNeverFails(){
+
+  let count=0;
+  while (Math.random() >= 0.5) {
+    count=count +1;
+    }
+
+
+    return (`You got ${count} tails in a row!`);
+}
