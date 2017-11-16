3ddf24340c1b406f68ad55e681d5ba6a47731399
Isaac Chabon <ichabon@me.com>
2017-11-16 00:23:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function printBadges(arr){
+  for(let i=0; i<arr.length; i++){
+    console.log(`Welcome ${arr[i]}! You are employee #${i+1}.`);
+  }
+  return arr;
+}
+function tailsNeverFails(){
+  let coinflip = Math.round(Math.random());
+  let numbertails = 0;
+  while(coinflip !== 0){
+    coinflip = Math.round(Math.random());
+    numbertails ++;
+    console.log(coinflip);
+  }
+  return `You got ${numbertails} tails in a row!`;
+}
