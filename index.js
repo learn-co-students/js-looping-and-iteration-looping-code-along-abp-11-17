0847da201fb46783fdefb2cb95cce9da273749b2
Kyle Loftin <kaloftin@hotmail.com>
2017-11-16 06:38:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const arr = ["Joe", "Gabe", 'Lisa', 'Kaitlin', 'Jan'];
+
+function printBadges(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`Welcome ${arr[i]}! You are employee #${i + 1}.`);
+  }
+  return arr;
+}
+
+function tailsNeverFails(){
+  let coin = 0;
+  while (Math.random() >= 0.5) {
+    coin = coin + 1;
+  }
+  return `You got ${coin} tails in a row!`;
+}
