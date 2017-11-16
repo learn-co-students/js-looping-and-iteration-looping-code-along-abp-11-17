82c6205729d1666e028af3c0654c7e35fdbe6759
Tommaso Padula <tommaso.padula@gmail.com>
2017-11-16 20:24:28 +0000

    Done.

--- index.js
+++ index.js
@@ -2,16 +2,18 @@
 //F1
 const names = ['Lisa', 'Kaitlin', 'Jan']
 function printBadges(names){
-  for(i = 0; i < names.length; i++) {
-    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
-  }
+  let i = 0;
+  for(i = 0; i < names.length; i++){
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`)
+  };
   return names;
 };
+
 //F2
-function tailsNeverFails() {
-  let tails = 0;
-  while(Math.random() >= 0.5){
-  tails++;
-  }
-  return `You got ${tails} tails in a row!`;
+function tailsNeverFails(){
+  let i = 0
+  while (Math.random() >= 0.5){
+    i++;
+  };
+  return `You got ${i} tails in a row!`;
 };
