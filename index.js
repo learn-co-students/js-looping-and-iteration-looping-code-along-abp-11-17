ab0d88344bddcb87586b870d8a42c8bf1a997166
Adil Asif <asifadilanis@gmail.com>
2017-11-17 15:26:32 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function printBadges(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(`Welcome ${array[i]}! You are employee #${i + 1}.`);
+  }
+  return array;
+}
+
+function tailsNeverFails() {
+  let numberFlips = 0;
+  let maybeTrue = true;
+  while (maybeTrue) {
+    maybeTrue = Math.random() >= 0.5;
+    numberFlips++;
+  }
+  return `You got ${numberFlips - 1} tails in a row!`;
+}
