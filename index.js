4ad961607d47af641235ed58df03adcd3834a5ad
Rob Goelz <robgoelz@gmail.com>
2017-11-18 20:49:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+// const employees = ['Ada', 'Brendan', 'Ali'];
+function printBadges (element) {
+  let empNo = 1;
+  for (let i = 0; i < element.length; i++) {
+     console.log(`Welcome ${element[i]}! You are employee #${empNo}.`);
+     empNo ++;
+  }
+  return element;
+}
+
+ function tailsNeverFails() {
+   let i = 0;
+   while (Math.random() >= 0.5) {
+     i++;
+   }
+   return (`You got ${i} tails in a row!`);
+ }
