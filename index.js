33e0b19ce80720863072cc81c2735bc675a35637
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-09 18:01:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function printBadges(element){
+
+ for(let i = 0 ; i < element.length ; i++)
+ {
+   console.log(`Welcome ${element[i]}! You are employee #${i+1}.`);
+
+ }
+return element;
+}
+printBadges(element);
+
+function tailsNeverFails() {
+ let i = 0 ;
+  while(Math.random() >= 0.5) {
+    i++;
+  }
+   return `You got ${i} tails in a row!`;
+}
