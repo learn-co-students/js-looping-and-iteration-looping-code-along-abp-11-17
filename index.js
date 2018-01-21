8b32fdff9232c03759eabc7a4f848d5b5f1733a3
Shawn T <shawn@huthunt.com>
2018-01-21 20:52:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function printBadges(array) {
+  let employeeNumber = 0;
+  for (i = 0; i < array.length; i++) {
+    employeeNumber++;
+    console.log(`Welcome ${array[i]}! You are employee #${employeeNumber}.`);
+
+  }
+  return array;
+}
+
+function tailsNeverFails() {
+  let tailsCount = 0
+  function coinFlip() {
+    return Math.random() >= .5;
+  }
+  while (coinFlip()) {
+    tailsCount++;
+  }
+  return `You got ${tailsCount} tails in a row!`;
+}
