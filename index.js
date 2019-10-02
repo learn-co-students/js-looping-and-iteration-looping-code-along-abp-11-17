79fcf7fb75c08570cbf41d0af4f2f17ba43823ea
ahotchkin <ahotchk1@yahoo.com>
2019-10-02 15:24:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 // Code your solutions in this file
-function printBadges(arrayNames){
-  for (let i = 0; i < arrayNames.length; i++){
-    console.log(`Welcome ${arrayNames[i]}! You are employee #${i+1}.`)
+function printBadges(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i+1}.`);
   }
-
-  return arrayNames;
+  return names;
 }
 
-function coinToss(){
-  return Math.random() >= 0.5;
-}
-
-function tailsNeverFails(){
-  let counter = 0;
-  while (coinToss()){
+function tailsNeverFails() {
+  let counter = 0
+  while (Math.random() >= 0.5) {
     counter++
   }
   return `You got ${counter} tails in a row!`
