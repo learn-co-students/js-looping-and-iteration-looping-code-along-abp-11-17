4bd62757c379258425d1c6b5aada91e4019cd0e9
kylejb <47502769+kylejb@users.noreply.github.com>
2020-08-01 20:07:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(name, eventName) {
+    let tyCard = []
+
+    for ( let i = 0; i < name.length; i++ ) {
+        console.log(i)
+        tyCard.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return tyCard;
+}
+
+function countDown(num) {
+
+    while (num > 0) {
+        console.log(num)
+        num-=1
+    }
+    console.log(num)
+}
